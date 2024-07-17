905fb33bec705a02084df04a44c79c4d381a8415
shwetha <skulal386@gmail.com>
2024-07-17 19:54:32 +0530

    coded example to render in console

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,1 +1,11 @@
-alert("Hello there!!")
+// alert("Hello there!!")
+console.log(123)
+console.log("Microdegree")
+console.log(true)
+
+var greeting="Hello";
+console.log(greeting);
+
+console.log([1,2,3,4,5])
+
+console.log({a:1,b:2,c:3})
